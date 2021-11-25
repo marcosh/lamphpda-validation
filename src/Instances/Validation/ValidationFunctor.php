29e3ce7b48e5555eae441b5a14c93459feb28527
marcosh <pasafama@gmail.com>
2021-11-25 17:00:18 +0100

    functor instance for validators

---
+++ src/Instances/Validation/ValidationFunctor.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\Validation\Instances\Validation;
+
+use Marcosh\LamPHPda\Either;
+use Marcosh\LamPHPda\HK\HK1;
+use Marcosh\LamPHPda\Typeclass\Functor;
+use Marcosh\LamPHPda\Validation\Brand\ValidationBrand;
+use Marcosh\LamPHPda\Validation\Validation;
+
+/**
+ * @implements Functor<ValidationBrand>
+ *
+ * @psalm-immutable
+ */
+final class ValidationFunctor implements Functor
+{
+    /**
+     * @template A
+     * @template B
+     * @template C
+     * @template E
+     * @param pure-callable(A): B $f
+     * @param HK1<ValidationBrand<C, E>, A> $a
+     * @return Validation<C, E, B>
+     *
+     * @psalm-pure
+     */
+    public function map(callable $f, HK1 $a): HK1
+    {
+        $aValidation = Validation::fromBrand($a);
+
+        return new Validation(
+            /**
+             * @param C $c
+             * @return Either<E, B>
+             */
+            fn($c) => $aValidation->validate($c)->map($f)
+        );
+    }
+}
